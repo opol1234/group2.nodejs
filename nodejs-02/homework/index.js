3b36a8cb95d96abc2cfb84aa67815155bbd415de
laksamee <laksamee.pr.57@ubu.ac.th>
2017-09-07 17:03:20 +0700

    02

--- nodejs-02/homework/index.js
+++ nodejs-02/homework/index.js
@@ -5,4 +5,5 @@ http.createServer(function (req, res) {
 	res.write('<h1 style="color:green">Hello</h1>');
 	res.write('<h3 style="color:blue">I am Patcharaporn Thongrong (5711404871)<h3>');
 	res.write('<h3 style="color:red">I am Laksamee Prathumwan (5711403366)</h3>');
+	res.write('<h3 style="color:yellow">Thank You!!</h3>');
 }).listen(8080);
