067143ff7020287f9822eb6ff1287e1aa439e962
opol1234 <patcharaporn.th.57@ubu.ac.th>
2017-09-07 09:56:44 +0700

    add index.js

--- nodejs-02/homework/index.js
+++ nodejs-02/homework/index.js
@@ -2,6 +2,7 @@ var http = require('http');
 
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end('Hello I am Patcharaporn Thongrong (5711404871)!');
-    res.end('Hello I am Laksamee Prathumwan (5711403366)!');
+	res.write('<h1 style="color:green">Hello</h1>');
+	res.write('<h3 style="color:blue">I am Patcharaporn Thongrong (5711404871)<h3>');
+	res.write('<h3 style="color:red">I am Laksamee Prathumwan (5711403366)</h3>');
 }).listen(8080);
